cc999b152f175f8d32404cdeb04e5aed78740a8a
DavJean <djdavjean@gmail.com>
2020-11-20 03:43:16 +0100

    added new classes

---
+++ src/com/company/TestFace.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class TestFace {
+    private int checkPrijs;
+    private  String showWay;
+    private  int Totaal;
+
+    public int getCheckPrijs() {
+        return checkPrijs;
+    }
+
+    public int getTotaal() {
+        return Totaal;
+    }
+
+    public String getShowWay() {
+
+        return showWay;
+    }
+
+
+
+
+
+}
